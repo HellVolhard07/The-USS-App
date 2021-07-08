a915a38c52dfa362e337ff275cf5ac24793faeee
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2021-07-08 14:12:30 +0530

    firebase init

---
+++ lib/screens/events_screen.dart
@@ -0,0 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:the_uss_project/constants.dart';
+
+class EventsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection(eventsCollection)
+            .orderBy("date", descending: true)
+            .snapshots(),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (snapshot.data == null) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          var eventsData = snapshot.data.docs;
+          // print(snapshot.data.docs[0]["title"]);
+          return ListView.builder(
+            itemBuilder: (ctx, index) => Center(
+              child: Text(
+                eventsData[index]["title"],
+              ),
+            ),
+            itemCount: eventsData.length,
+          );
+        },
+      ),
+    );
+  }
+}
