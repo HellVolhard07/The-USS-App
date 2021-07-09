fd8757ab34e714d56f9ed68e2f63120942605969
shubham-rawal <shubhamrawal2001@gmail.com>
2021-07-09 16:47:26 +0530

    ui updates

--- lib/screens/event_screen.dart
+++ lib/screens/event_screen.dart
@@ -6,8 +6,28 @@ class EventScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final eventArgs = ModalRoute.of(context)!.settings.arguments as EventItem;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(eventArgs.eventTitle),
+      // appBar: AppBar(
+      //   title: Text(eventArgs.eventTitle),
+      // ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20.0, 20, 20, 17),
+              child: Text(
+                eventArgs.eventTitle,
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w900),
+              ),
+            ),
+            Divider(
+              // indent: 20,
+              endIndent: 20,
+              thickness: 3.0,
+              color: Colors.deepPurpleAccent,
+            ),
+          ],
+        ),
       ),
     );
   }
