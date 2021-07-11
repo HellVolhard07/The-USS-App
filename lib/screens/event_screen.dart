afc5bdfeffa37d77a1a2b402a3a95da7bbabaa31
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2021-07-11 17:04:11 +0530

    animation on page route

--- lib/screens/event_screen.dart
+++ lib/screens/event_screen.dart
@@ -1,30 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:the_uss_project/widgets/event_item.dart';
 
+import '../widgets/sliver_header.dart';
+
 class EventScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final eventArgs = ModalRoute.of(context)!.settings.arguments as EventItem;
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text(eventArgs.eventTitle),
-      // ),
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20.0, 20, 20, 17),
-              child: Text(
-                eventArgs.eventTitle,
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w900),
-              ),
-            ),
-            Divider(
-              // indent: 20,
-              endIndent: 20,
-              thickness: 3.0,
-              color: Colors.deepPurpleAccent,
+        child: CustomScrollView(
+          slivers: [
+            sliverHeader(
+              eventArgs.eventTitle,
+              eventArgs.eventPosterUrl,
             ),
           ],
         ),
