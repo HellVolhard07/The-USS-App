706b19e6fa24ee4b477355f73f8a05ff027fe812
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2021-07-12 15:33:54 +0530

    login screen ui

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
 import 'package:the_uss_project/screens/events_screen.dart';
+import 'package:the_uss_project/screens/login_screen.dart';
 import 'package:the_uss_project/screens/society_list_screen.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -12,6 +13,7 @@ class HomeScreen extends StatelessWidget {
     return [
       EventsScreen(),
       SocietyListScreen(),
+      LoginScreen(),
     ];
   }
 
@@ -32,6 +34,15 @@ class HomeScreen extends StatelessWidget {
         textStyle: TextStyle(
           fontWeight: FontWeight.bold,
           fontSize: 20,
+        ),
+        activeColorPrimary: Colors.deepPurpleAccent,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.account_circle_outlined),
+        title: 'Account',
+        textStyle: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 12,
         ),
         activeColorPrimary: Colors.deepPurpleAccent,
       ),
