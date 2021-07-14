f228afb2177be30fc64f45d516570a0cde4dd235
Ryuken07 <itsmevasu84@gmail.com>
2021-07-14 14:14:54 +0530

    Bug fixes and error dialog added

--- lib/widgets/auth.dart
+++ lib/widgets/auth.dart
@@ -1,20 +1,27 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:the_uss_project/widgets/show_alert_dialogue.dart';
 
 class LoginProvider with ChangeNotifier {
   FirebaseAuth _auth = FirebaseAuth.instance;
 
   FirebaseAuth get getAuth => _auth;
 
-  Future loginUser(String email, String password) async {
+  late String error;
+
+  String get getError => error;
+
+  Future loginUser(String email, String password, BuildContext context) async {
     try {
       UserCredential userCredential = await FirebaseAuth.instance
           .signInWithEmailAndPassword(email: email, password: password);
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
-        print('No user found for that email.');
+        error = "User not found";
+        showMyDialog(context, error);
       } else if (e.code == 'wrong-password') {
-        print('Wrong password provided for that user.');
+        error = "Incorrect password";
+        showMyDialog(context, error);
       }
     }
   }
