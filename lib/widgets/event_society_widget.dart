e757abc130df2a5d6611549d0a01e0e0301cd3d4
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2021-07-29 13:32:42 +0530

    society events sorted

--- lib/widgets/event_society_widget.dart
+++ lib/widgets/event_society_widget.dart
@@ -6,7 +6,6 @@ import 'package:the_uss_project/constants.dart';
 import 'package:the_uss_project/theme_provider.dart';
 import 'package:the_uss_project/widgets/society_item.dart';
 
-import 'auth.dart';
 import 'event_item.dart';
 
 class EventSocietyWidget extends StatefulWidget {
@@ -17,11 +16,23 @@ class EventSocietyWidget extends StatefulWidget {
 class _EventSocietyWidgetState extends State<EventSocietyWidget> {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
+  List sortByDate(List event) {
+    event.forEach((element) {
+      element[date].toDate();
+    });
+
+    event.sort((e1, e2) => e2[date].compareTo(e1[date]));
+
+    return event;
+  }
+
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
     final societyArgs =
         ModalRoute.of(context)!.settings.arguments as SocietyItem;
+
+    sortByDate(societyArgs.societyKeEvents);
     return Container(
       padding: EdgeInsets.all(0),
       decoration: BoxDecoration(
